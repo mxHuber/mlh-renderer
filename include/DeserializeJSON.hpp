a46011999268f91bc80f748f8083e22085c06983
mxHuber <huber.maximilian.leo@gmail.com>
2025-02-25 19:15:13 +0100

    fixed deser bugs

--- include/DeserializeJSON.hpp
+++ include/DeserializeJSON.hpp
@@ -2,12 +2,19 @@
 #define MLH_RENDERER_DESERIALIZE_JSON_HPP
 
 #include "Renderer.hpp"
+#include <nlohmann/json.hpp>
 
 namespace mlh {
 
 class DeserializeJSON {
 public:
-  void static deserializeJSON(const std::string &Path, Renderer &App);
+  static void deserializeJSON(const std::string &Path, Renderer &App);
+
+private:
+  static void loadColor(nlohmann::json JSON, float (&Color)[4]);
+  static void loadPosition(const nlohmann::json &JSON, float (&Position)[3]);
+  static void loadTexCoords(const nlohmann::json &JSON, float (&TexCoords)[3]);
+  static void loadQuadButtons(const nlohmann::json &JSON, Renderer &App);
 };
 
 } // namespace mlh
--- lib/DeserializeJSON.cpp
+++ lib/DeserializeJSON.cpp
@@ -1,7 +1,9 @@
 #include "DeserializeJSON.hpp"
+#include "Quad.hpp"
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <string>
 
 namespace mlh {
 
@@ -19,137 +21,137 @@ void loadBackground(const nlohmann::json &JSON, Renderer &App) {
   App.setBackground(BackgroundImageID);
 }
 
-void loadColor(const nlohmann::json &JSON, float (&Color)[4]) {
+void DeserializeJSON::loadColor(nlohmann::json JSON, float (&Color)[4]) {
   // 0
-  if (!JSON[0].is_number_float()) {
+  if (!JSON[0].is_number()) {
     std::cout
         << "[loadColor] WARNING: JSON[0] is not a float! Defaulting to 0.0f"
         << std::endl;
     Color[0] = 0.0f;
   } else {
-    Color[0] = float(JSON[0]);
+    Color[0] = JSON[0];
   }
 
   // 1
-  if (!JSON[1].is_number_float()) {
+  if (!JSON[1].is_number()) {
     std::cout
         << "[loadColor] WARNING: JSON[1] is not a float! Defaulting to 0.0f"
         << std::endl;
     Color[1] = 0.0f;
   } else {
-    Color[1] = float(JSON[1]);
+    Color[1] = JSON[1];
   }
 
   // 2
-  if (!JSON[2].is_number_float()) {
+  if (!JSON[2].is_number()) {
     std::cout
         << "[loadColor] WARNING: JSON[2] is not a float! Defaulting to 0.0f"
-        << std::endl;
+        << "JSON[2]: " << JSON[2] << std::endl;
     Color[2] = 0.0f;
   } else {
-    Color[2] = float(JSON[2]);
+    Color[2] = JSON[2];
   }
 
   // 3
-  if (!JSON[3].is_number_float()) {
+  if (!JSON[3].is_number()) {
     std::cout
         << "[loadColor] WARNING: JSON[3] is not a float! Defaulting to 0.0f"
-        << std::endl;
+        << "JSON[3]: " << JSON[3] << std::endl;
     Color[3] = 0.0f;
   } else {
-    Color[3] = float(JSON[3]);
+    Color[3] = JSON[3];
   }
 }
 
-void loadPosition(const nlohmann::json &JSON, float (&Position)[3]) {
+void DeserializeJSON::loadPosition(const nlohmann::json &JSON,
+                                   float (&Position)[3]) {
   // 0
-  if (!JSON[0].is_number_float()) {
+  if (!JSON[0].is_number()) {
     std::cout << "[loadPosition()] WARNING: JSON[0] is not a float! Defaulting "
                  "to 0.0f"
               << std::endl;
     Position[0] = 0.0f;
   } else {
-    Position[0] = float(JSON[0]);
+    Position[0] = JSON[0];
   }
 
   // 1
-  if (!JSON[1].is_number_float()) {
+  if (!JSON[1].is_number()) {
     std::cout << "[loadPosition()] WARNING: JSON[1] is not a float! Defaulting "
                  "to 0.0f"
               << std::endl;
     Position[1] = 0.0f;
   } else {
-    Position[1] = float(JSON[1]);
+    Position[1] = JSON[1];
   }
 
   // 2
-  if (!JSON[2].is_number_float()) {
+  if (!JSON[2].is_number()) {
     std::cout << "[loadPosition()] WARNING: JSON[2] is not a float! Defaulting "
                  "to 0.0f"
               << std::endl;
     Position[2] = 0.0f;
   } else {
-    Position[2] = float(JSON[2]);
+    Position[2] = JSON[2];
   }
 }
 
-void loadTexCoords(const nlohmann::json &JSON, float (&TexCoords)[3]) {
+void DeserializeJSON::loadTexCoords(const nlohmann::json &JSON,
+                                    float (&TexCoords)[3]) {
   // 0
-  if (!JSON[0].is_number_float()) {
+  if (!JSON[0].is_number()) {
     std::cout
         << "[loadTexCoords()] WARNING: JSON[0] is not a float! Defaulting "
            "to 0.0f"
         << std::endl;
     TexCoords[0] = 0.0f;
   } else {
-    TexCoords[0] = float(JSON[0]);
+    TexCoords[0] = JSON[0];
   }
 
   // 1
-  if (!JSON[1].is_number_float()) {
+  if (!JSON[1].is_number()) {
     std::cout
         << "[loadTexCoords()] WARNING: JSON[1] is not a float! Defaulting "
            "to 0.0f"
         << std::endl;
     TexCoords[1] = 0.0f;
   } else {
-    TexCoords[1] = float(JSON[1]);
+    TexCoords[1] = JSON[1];
   }
 
   // 2
-  if (!JSON[2].is_number_float()) {
+  if (!JSON[2].is_number()) {
     std::cout
         << "[loadTexCoords()] WARNING: JSON[2] is not a float! Defaulting "
            "to 0.0f"
-        << std::endl;
+        << "\nJSON[2]: " << JSON[2] << std::endl;
     TexCoords[2] = 0.0f;
   } else {
-    TexCoords[2] = float(JSON[2]);
+    TexCoords[2] = JSON[2];
   }
 }
 
-void loadQuadButtons(const nlohmann::json &JSON, Renderer &App) {
-  int Counter = 0;
+void DeserializeJSON::loadQuadButtons(const nlohmann::json &JSON,
+                                      Renderer &App) {
   for (const auto &QuadButton : JSON["QuadButtons"]) {
-    std::cout << "QuadButton " << Counter << ": " << QuadButton << std::endl;
-    Counter++;
     Quad CurrentQuad = Quad();
 
-    loadColor(JSON["A"]["Color"], CurrentQuad.A.Color);
-    loadPosition(JSON["A"]["Position"], CurrentQuad.A.Position);
-    loadTexCoords(JSON["A"]["TexCoords"], CurrentQuad.A.TexCoords);
+    loadColor(QuadButton["A"]["Color"], CurrentQuad.A.Color);
+    loadPosition(QuadButton["A"]["Position"], CurrentQuad.A.Position);
+    loadTexCoords(QuadButton["A"]["TexCoords"], CurrentQuad.A.TexCoords);
 
-    loadColor(JSON["B"]["Color"], CurrentQuad.B.Color);
-    loadPosition(JSON["B"]["Position"], CurrentQuad.B.Position);
-    loadTexCoords(JSON["B"]["TexCoords"], CurrentQuad.B.TexCoords);
+    loadColor(QuadButton["B"]["Color"], CurrentQuad.B.Color);
+    loadPosition(QuadButton["B"]["Position"], CurrentQuad.B.Position);
+    loadTexCoords(QuadButton["B"]["TexCoords"], CurrentQuad.B.TexCoords);
 
-    loadColor(JSON["C"]["Color"], CurrentQuad.C.Color);
-    loadPosition(JSON["C"]["Position"], CurrentQuad.C.Position);
-    loadTexCoords(JSON["C"]["TexCoords"], CurrentQuad.C.TexCoords);
+    loadColor(QuadButton["C"]["Color"], CurrentQuad.C.Color);
+    loadPosition(QuadButton["C"]["Position"], CurrentQuad.C.Position);
+    loadTexCoords(QuadButton["C"]["TexCoords"], CurrentQuad.C.TexCoords);
 
-    loadColor(JSON["D"]["Color"], CurrentQuad.D.Color);
-    loadPosition(JSON["D"]["Position"], CurrentQuad.D.Position);
-    loadTexCoords(JSON["D"]["TexCoords"], CurrentQuad.D.TexCoords);
+    loadColor(QuadButton["D"]["Color"], CurrentQuad.D.Color);
+    loadPosition(QuadButton["D"]["Position"], CurrentQuad.D.Position);
+    loadTexCoords(QuadButton["D"]["TexCoords"], CurrentQuad.D.TexCoords);
 
     App.createQuadButton(CurrentQuad);
   }
